f9e4b1310a3ff26a19ef11b4dbe60ea9c71125c0
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2019-03-25 17:43:51 +0100

    BitConDec-11: Update License and readme

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # cures-condec-bitbucket
-Bitbucket plugin to integrate decision knowledge into pull requests
+
+The CURES ConDec Bitbucket plugin provides decision knowledge support for pull requests. 
+A pull request and its associated feature branch are related to one (or more) JIRA issues. 
+The ConDec Bitbucket plugin enables developers to view decision knowledge relevant for the JIRA issue(s) directly within the pull request.
+This supports the developers in keeping the documented decision knowledge and the implementation of decisions consistent.
+Further, the plugin checks and enforces the completeness of the documented decision knowledge prior to merging.
+That is, the branch can only be merged back to mainline, if the decision knowledge has a certain quality.
+For example, the developers responsible for implementing a task must have at least one issue (decision problem) and one decision documented (will be configurable by the rationale manager in the future).
+
+## Installation
+
+### Prerequisites
+The following prerequisites are necessary to compile the plug-in from source code:
+- Java 8 JDK
+- [Atlassian SDK](https://developer.atlassian.com/docs/getting-started/set-up-the-atlassian-plugin-sdk-and-build-a-project)
+
+### Compilation via Terminal
+The source code is compiled via terminal.
+Navigate into the cures-condec-jira folder and run the following command:
+```
+atlas-mvn package
+```
+The .jar file is created.
+
+Run the plug-in locally via:
+```
+atlas-run
+```
+
+### Download of Precompiled .jar-File
+The precompiled .jar-File for the latest release can be found here: https://github.com/cures-hub/cures-condec-bitbucket/releases/latest
--- src/main/java/de/uhd/ifi/se/decision/management/bitbucket/merge/checks/HasDecisionKnowledgeCheck.java
+++ src/main/java/de/uhd/ifi/se/decision/management/bitbucket/merge/checks/HasDecisionKnowledgeCheck.java
@@ -1,32 +1,26 @@
 package de.uhd.ifi.se.decision.management.bitbucket.merge.checks;
 
-import com.atlassian.bitbucket.hook.repository.*;
-import com.atlassian.bitbucket.i18n.I18nService;
-import com.atlassian.bitbucket.permission.Permission;
-import com.atlassian.bitbucket.permission.PermissionService;
-import com.atlassian.bitbucket.repository.Repository;
-import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
-import de.uhd.ifi.se.decision.management.bitbucket.oAuth.ApiLinkService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import com.atlassian.bitbucket.pull.PullRequest;
-import com.atlassian.bitbucket.pull.PullRequestRef;
-
-import com.atlassian.bitbucket.commit.CommitsBetweenRequest;
-import com.atlassian.bitbucket.commit.Commit;
-import com.atlassian.bitbucket.commit.CommitService;
-import com.atlassian.bitbucket.commit.SimpleCommit;
+import java.util.HashMap;
 
 import javax.annotation.Nonnull;
 
-import com.atlassian.bitbucket.util.PageRequestImpl;
-import com.atlassian.bitbucket.util.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-import java.util.*;
+import com.atlassian.bitbucket.commit.Commit;
+import com.atlassian.bitbucket.commit.CommitService;
+import com.atlassian.bitbucket.hook.repository.PreRepositoryHookContext;
+import com.atlassian.bitbucket.hook.repository.PullRequestMergeHookRequest;
+import com.atlassian.bitbucket.hook.repository.RepositoryHookResult;
+import com.atlassian.bitbucket.hook.repository.RepositoryMergeCheck;
+import com.atlassian.bitbucket.pull.PullRequest;
+import com.atlassian.bitbucket.pull.PullRequestRef;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+
+import de.uhd.ifi.se.decision.management.bitbucket.oAuth.ApiLinkService;
 
 @Component("hasDecisionKnowledgeCheck")
 public class HasDecisionKnowledgeCheck implements RepositoryMergeCheck {
-
 
 	private final CommitService commitService;
 	public static String JIRA_QUERY;
@@ -40,31 +34,36 @@ public class HasDecisionKnowledgeCheck implements RepositoryMergeCheck {
 	@Nonnull
 	@Override
 	public RepositoryHookResult preUpdate(@Nonnull PreRepositoryHookContext context,
-										  @Nonnull PullRequestMergeHookRequest request) {
+			@Nonnull PullRequestMergeHookRequest request) {
 
-		//get pullRequest
+		// get pullRequest
 		final PullRequest pullRequest = request.getPullRequest();
 
-		//find correct query out of projects, commitMessages and BranchId
+		// find correct query out of projects, commitMessages and BranchId
 		MergeCheckDataHandler mergeCheckDataHandler = new MergeCheckDataHandler();
 		Iterable<Commit> commits = mergeCheckDataHandler.getCommitsOfPullRequest(pullRequest, this.commitService);
 		String branchTitle = pullRequest.getTitle();
 		PullRequestRef pullRequestRef = pullRequest.getFromRef();
 		String branchId = pullRequestRef.getDisplayId();
-		String queryWithJiraIssues = "?jql=key in " + mergeCheckDataHandler.getJiraCallQuery(commits, branchTitle, branchId);
+		String queryWithJiraIssues = "?jql=key in "
+				+ mergeCheckDataHandler.getJiraCallQuery(commits, branchTitle, branchId);
 		String projectString = ApiLinkService.getCurrentActiveJiraProjects();
 		if (!mergeCheckDataHandler.checkIfJsonArray(projectString)) {
 			return RepositoryHookResult.accepted();
 		} else {
 			JIRA_QUERY = queryWithJiraIssues;
-			PROJECT_KEY = mergeCheckDataHandler.getProjectKeyFromJiraAndCheckWhichOneCouldBe(commits, projectString, branchId, branchTitle);
+			PROJECT_KEY = mergeCheckDataHandler.getProjectKeyFromJiraAndCheckWhichOneCouldBe(commits, projectString,
+					branchId, branchTitle);
 
-			//get Issues out of Jira
-			String decisionKnowledgeIssueString = ApiLinkService.getDecisionKnowledgeFromJira(queryWithJiraIssues, PROJECT_KEY);
-			HashMap<String, String> hasSufficientDecisions = mergeCheckDataHandler.hasSufficientDecisions(decisionKnowledgeIssueString);
+			// get Issues out of Jira
+			String decisionKnowledgeIssueString = ApiLinkService.getDecisionKnowledgeFromJira(queryWithJiraIssues,
+					PROJECT_KEY);
+			HashMap<String, String> hasSufficientDecisions = mergeCheckDataHandler
+					.hasSufficientDecisions(decisionKnowledgeIssueString);
 			if (!Boolean.valueOf(hasSufficientDecisions.get("resultBoolean"))) {
 				String summaryMsg = "There are not enough Decision Elements for each Commit";
-				String detailedMsg = "Every Commit which is linked to a jira Ticket has to have at least one issue and one decision linked :" + hasSufficientDecisions.get("resultString");
+				String detailedMsg = "Every Commit which is linked to a jira Ticket has to have at least one issue and one decision linked :"
+						+ hasSufficientDecisions.get("resultString");
 
 				return RepositoryHookResult.rejected(summaryMsg, detailedMsg);
 			} else {
