ca961f7b009065f5ace0a2becab789b676de392d
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2019-10-05 17:10:14 +0200

    Add eslintrc to configure Javascript code quality checks

--- src/main/resources/js/has-decision-knowledge-check.js
+++ src/main/resources/js/has-decision-knowledge-check.js
@@ -13,51 +13,50 @@
 	UHD.ISSUES.callInitIssues = callInitIssues;
 
 	checkIfListIsLoaded(0);
+	
+	function showText(text) {
+		$('.todo-list').replaceWith("<div class'todo-list'><p>" + text + "</p></div>");
+	}
+
+	function insertDecisionKnowledgeButton(oIssues) {
+		$('.todo-list').replaceWith(
+				"<button class='aui aui-button' id='showDecisionKnowledgeButton'>Show decision knowledge</button>");
+		$("#showDecisionKnowledgeButton").click(function() {
+			openDialog(oIssues);
+		});
+	}
+
+	function openDialog(oIssues) {
+		// Standard sizes are 400, 600, 800 and 960 pixels wide
+		var dialog = new AJS.Dialog({
+			width : 800,
+			height : 500,
+			id : "example-dialog",
+			closeOnOutsideClick : true
+		});
+		dialog.addHeader("Decision Knowledge");
+		dialog.addPanel("Panel 1", "<div id='decisionKnowledgeTableDiv'></div>", "panel-body");
+		$("#decisionKnowledgeTableDiv").replaceWith(UHD.issues.issueList({
+			objects : oIssues
+		}));
+		dialog.addButton("Close", function(dialog) {
+			dialog.hide();
+		});
+
+		dialog.gotoPage(0);
+		dialog.gotoPanel(0);
+		dialog.show();
+	}
 
 	function getIssuesFromJira() {
-		conDecAPI
-				.getDecisionKnowledgeFromJira(function(data) {
-					// openDialog();
-					try {
-						var oIssues = JSON.parse(data);
-						if (oIssues.length > 0) {
-							$('.todo-list')
-									.replaceWith(
-											"<button class='aui aui-button' id='showDecisionKnowledgeButton'>Show decision knowledge</button>");
-							$("#showDecisionKnowledgeButton").click(function() {
-								openDialog(oIssues);
-							})
-						} else {
-							$('.todo-list')
-									.replaceWith(
-											"<div class'todo-list'><p>None of the commit messages, branch-id or branch-title could be linked to a Jira issue. Continue with the merge on your own risk</p></div>")
-						}
-					} catch (e) {
-						$('.todo-list').replaceWith("<div class'todo-list'><p>" + data + "</p></div>")
-					}
-				});
-
-		function openDialog(oIssues) {
-			// Standard sizes are 400, 600, 800 and 960 pixels wide
-			var dialog = new AJS.Dialog({
-				width : 800,
-				height : 500,
-				id : "example-dialog",
-				closeOnOutsideClick : true
-			});
-			dialog.addHeader("Decision Knowledge");
-			dialog.addPanel("Panel 1", "<div id='decisionKnowledgeTableDiv'></div>", "panel-body");
-			$("#decisionKnowledgeTableDiv").replaceWith(UHD.issues.issueList({
-				objects : oIssues
-			}));
-			dialog.addButton("Close", function(dialog) {
-				dialog.hide();
-			});
-
-			dialog.gotoPage(0);
-			dialog.gotoPanel(0);
-			dialog.show();
-		}
+		conDecAPI.getDecisionKnowledgeFromJira(function(data) {
+			var oIssues = JSON.parse(data);
+			if (oIssues.length == 0) {
+				showText("None of the commit messages, branch-id or branch-title could be linked to a Jira issue.");
+				return;
+			}
+			insertDecisionKnowledgeButton(oIssues);
+		});
 	}
 
 	function checkIfListIsLoaded(iCounter) {
