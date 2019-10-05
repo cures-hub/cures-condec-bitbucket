553e6de0c92f7bd1442dec3ec45980af5867fbaf
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2019-10-05 23:44:22 +0200

    BitConDec-11: Add dialog HTML code in soy template (#3)
    * Make table sortable
    
    * Rename client-web-panel method from UHD to ConDec
    
    * Rename has-decision-knowledge-check.js to condec.knowledge.overview.js
    
    * Show table header only once (we might revert this in the future)
    
    * Remove description of knowledge element from knowledge overview since it takes too much space
    
    * Add dialog HTML code to soy template
    
    * Fill overview dialog already in init function of condec.knowledge.overview.js

--- src/main/resources/js/condec.api.js
+++ src/main/resources/js/condec.api.js
@@ -5,10 +5,10 @@
  * nothing
     
  Is required by
- * ???
+ * condec.knowledge.overview.js
   
  Is referenced in HTML by
- * ???
+ * nowhere
  */
 (function(global) {
 
@@ -20,36 +20,27 @@
 	 * external references:
 	 */
 	ConDecAPI.prototype.getDecisionKnowledgeFromJira = function getDecisionKnowledgeFromJira(callback) {
-		var url = this.restPrefix + "/knowledge/getDecisionKnowledgeFromJira";
-		getJSON(
-				url,
-				function(error, decisionKnowledgeElements) {
-					if (error === null) {
-						callback(decisionKnowledgeElements);
-					} else {
-						$(".todo-list")
-								.replaceWith(
-										"<div class'todo-list'><p>There seems to be a problem with the connection to Jira "
-												+ "or none of the commit messages, branch-id or branch-title could be linked to a Jira issue.</p></div>")
-					}
-				});
+		var url = this.restPrefix + "/knowledge/getDecisionKnowledgeFromJira";		
+		return getResponseAsReturnValue(url);
 	};
 
-	function getJSON(url, callback) {
+	function getResponseAsReturnValue(url) {
 		var xhr = new XMLHttpRequest();
-		xhr.open("GET", url, true);
+		xhr.open("GET", url, false);
 		xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
-		xhr.setRequestHeader("Accept", "application/json");
-		xhr.onload = function() {
-			var status = xhr.status;
-			if (status === 200) {
-				callback(null, xhr.responseText);
-			} else {
-				showFlag("error", xhr.response.error, status);
-				callback(status);
-			}
-		};
 		xhr.send();
+		var status = xhr.status;
+		if (status === 200) {
+			try {
+				var parsedResponse = JSON.parse(xhr.response);
+				return parsedResponse;
+			} catch (error) {
+				console.log(error);
+				return null;
+			}
+		}
+		showFlag("error", xhr.response.error, status);
+		return null;
 	}
 
 	function showFlag(type, message, status) {
---
+++ src/main/resources/js/condec.knowledge.overview.js
@@ -0,0 +1,68 @@
+/*
+ This module creates the decision knowledge overview that shows the related knowledge for the pull request.
+
+ Requires
+ * condec.api.js
+ * condec.knowledge.overview.soy
+    
+ Is required by
+ * atlassian-plugin.xml calls ConDec.knowledgeOverview.init in the client-web-panel
+  
+ Is referenced in HTML by
+ * nowhere
+ */
+(function($) {
+	// Set up our namespace
+	window.ConDec = window.ConDec || {};
+	ConDec.knowledgeOverview = ConDec.knowledgeOverview || {};
+
+	/*
+	 * Exposes the client-condition function. external references:
+	 * client-web-panel in atlassian-plugin.xml
+	 */
+	ConDec.knowledgeOverview.init = function init() {
+		knowledgeElements = conDecAPI.getDecisionKnowledgeFromJira(null);
+		console.log(knowledgeElements);
+		if (knowledgeElements === null) {
+			return {
+				objects : null
+			};
+		}
+
+		return {
+			objects : knowledgeElements
+		};
+	};
+
+	checkIfSoyTemplateIsRendered(0);
+
+	function checkIfSoyTemplateIsRendered(iCounter) {
+		// check if inital List exists
+		if ($(".todo-list").length > 0) {
+			setEventListeners();
+		} else {
+			if (iCounter < 10) {
+				setTimeout(function() {
+					// max 10 times, else an error occured
+					checkIfSoyTemplateIsRendered(iCounter + 1);
+				}, 3000);
+			}
+		}
+	}
+
+	function setEventListeners() {
+		$("#showDecisionKnowledgeButton").click(function() {
+			// Show dialog
+			AJS.dialog2("#knowledge-overview-dialog").show();
+		});
+
+		$("#knowledge-overview-dialog-cancel-button").click(function() {
+			AJS.dialog2("#knowledge-overview-dialog").hide();
+		});
+	}
+
+	function showText(text) {
+		$(".todo-list").replaceWith("<div class'todo-list'><p>" + text + "</p></div>");
+	}
+
+}(AJS.$));
