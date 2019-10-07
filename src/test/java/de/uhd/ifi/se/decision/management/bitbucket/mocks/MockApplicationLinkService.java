82ddc26365a907ee339227a6e12d39b0d5e0941e
Anja Kleebaum <Anja.Kleebaum@informatik.uni-heidelberg.de>
2019-10-07 20:49:02 +0200

    Mock Jira ApplicationLink and test method getCurrentActiveJiraProjects()

--- src/test/java/de/uhd/ifi/se/decision/management/bitbucket/mocks/MockApplicationLinkService.java
+++ src/test/java/de/uhd/ifi/se/decision/management/bitbucket/mocks/MockApplicationLinkService.java
@@ -1,5 +1,8 @@
 package de.uhd.ifi.se.decision.management.bitbucket.mocks;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.atlassian.applinks.api.ApplicationId;
 import com.atlassian.applinks.api.ApplicationLink;
 import com.atlassian.applinks.api.ApplicationLinkService;
@@ -8,28 +11,31 @@ import com.atlassian.applinks.api.TypeNotInstalledException;
 
 public class MockApplicationLinkService implements ApplicationLinkService {
 
+	private List<ApplicationLink> applicationLinks;
+
+	public MockApplicationLinkService() {
+		this.applicationLinks = new ArrayList<ApplicationLink>();
+		this.applicationLinks.add(new MockApplicationLink());
+	}
+
 	@Override
 	public ApplicationLink getApplicationLink(ApplicationId arg0) throws TypeNotInstalledException {
-		// TODO Auto-generated method stub
-		return null;
+		return applicationLinks.get(0);
 	}
 
 	@Override
 	public Iterable<ApplicationLink> getApplicationLinks() {
-		// TODO Auto-generated method stub
-		return null;
+		return applicationLinks;
 	}
 
 	@Override
 	public Iterable<ApplicationLink> getApplicationLinks(Class<? extends ApplicationType> arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		return applicationLinks;
 	}
 
 	@Override
 	public ApplicationLink getPrimaryApplicationLink(Class<? extends ApplicationType> arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		return applicationLinks.get(0);
 	}
 
 }
--- src/test/java/de/uhd/ifi/se/decision/management/bitbucket/oauth/TestJiraClient.java
+++ src/test/java/de/uhd/ifi/se/decision/management/bitbucket/oauth/TestJiraClient.java
@@ -1,6 +1,9 @@
 package de.uhd.ifi.se.decision.management.bitbucket.oauth;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+
+import java.util.Set;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -12,15 +15,24 @@ import de.uhd.ifi.se.decision.management.bitbucket.mocks.MockCommitService;
 import de.uhd.ifi.se.decision.management.bitbucket.oauth.impl.JiraClientImpl;
 
 public class TestJiraClient {
+	
+	private static JiraClient jiraClient;
 
 	@BeforeClass
 	public static void setUp() {
 		MockComponentLocator.create(new MockCommitService(), new MockApplicationLinkService());
+		jiraClient = new JiraClientImpl();
 	}
 
 	@Test
 	public void testConstructor() {
 		assertNotNull(new JiraClientImpl());
+	}
+	
+	@Test
+	public void testGetCurrentActiveJiraProjects() {
+		Set<String> projectKeys = jiraClient.getCurrentActiveJiraProjects();
+		assertEquals(1, projectKeys.size());
 	}
 
 }
